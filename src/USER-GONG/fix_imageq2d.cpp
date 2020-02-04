48f215cdd1741eefaef789a3aedd96740804964b
Zheng Gong <unimisia@gmail.com>
2020-02-04 17:38:46 +0100

    Add image charge method with Petersen's approach

--- src/USER-GONG/fix_imageq2d.cpp
+++ src/USER-GONG/fix_imageq2d.cpp
@@ -15,7 +15,7 @@
    Contributing author: Zheng GONG (ENS de Lyon)
 ------------------------------------------------------------------------- */
 
-#include "fix_imagecharge.h"
+#include "fix_imageq2d.h"
 #include <cmath>
 #include <cstring>
 #include "atom.h"
@@ -34,9 +34,7 @@ using namespace FixConst;
 
 /* ---------------------------------------------------------------------- */
 
-FixImageCharge::FixImageCharge(LAMMPS *lmp, int narg, char **arg) :
-        Fix(lmp, narg, arg),
-        group2(NULL) {
+FixImageQ2D::FixImageQ2D(LAMMPS *lmp, int narg, char **arg) : Fix(lmp, narg, arg) {
   scalar_flag = 0;
   vector_flag = 0;
   size_vector = 4;
@@ -45,19 +43,34 @@ FixImageCharge::FixImageCharge(LAMMPS *lmp, int narg, char **arg) :
   extvector = 1;
   dynamic_group_allow = 0;
 
-  if (narg != 5) error->all(FLERR, "Illegal fix imgq command");
+  if (narg != 9) error->all(FLERR, "Illegal fix imgq2d command");
 
-  int n = strlen(arg[3]) + 1;
-  group2 = new char[n];
-  strcpy(group2, arg[3]);
   igroup2 = group->find(arg[3]);
   if (igroup2 == -1)
-    error->all(FLERR, "Fix imgq active group ID does not exist");
-  if (group->count(igroup) != group->count(igroup2))
-    error->all(FLERR, "Fix imgq number of atoms in image charge group does not equal to active group");
+    error->all(FLERR, "Fix imgq2d image group ID does not exist");
+  if (group->count(igroup) * 2 != group->count(igroup2))
+    error->all(FLERR, "Fix imgq2d number of atoms in image group does not equal to two times of active group");
   group2bit = group->bitmask[igroup2];
 
-  z0 = force->numeric(FLERR, arg[4]);
+  V = force->numeric(FLERR, arg[4]);
+
+  igroup_cathode = group->find(arg[5]);
+  if (igroup_cathode == -1)
+    error->all(FLERR, "Fix imgq2d cathode group ID does not exist");
+  groupbit_cathode = group->bitmask[igroup_cathode];
+
+  z_cathode = force->numeric(FLERR, arg[6]);
+
+  igroup_anode = group->find(arg[7]);
+  if (igroup_anode == -1)
+    error->all(FLERR, "Fix imgq2d anode group ID does not exist");
+  groupbit_anode= group->bitmask[igroup_anode];
+
+  z_anode = force->numeric(FLERR, arg[8]);
+
+  z_span = z_anode - z_cathode;
+  n_cathode = group->count(igroup_cathode);
+  n_anode = group->count(igroup_anode);
 
   memory->create(img_parent, atom->natoms + 1, "fix_image_charge::img_parent");
   memory->create(xyz, atom->natoms + 1, 3, "fix_image_charge::xyz");
@@ -69,8 +82,7 @@ FixImageCharge::FixImageCharge(LAMMPS *lmp, int narg, char **arg) :
 
 /* ---------------------------------------------------------------------- */
 
-FixImageCharge::~FixImageCharge() {
-  delete[] group2;
+FixImageQ2D::~FixImageQ2D() {
   memory->destroy(img_parent);
   memory->destroy(xyz);
   memory->destroy(xyz_local);
@@ -81,7 +93,7 @@ FixImageCharge::~FixImageCharge() {
 
 /* ---------------------------------------------------------------------- */
 
-int FixImageCharge::setmask() {
+int FixImageQ2D::setmask() {
   int mask = 0;
   mask |= PRE_EXCHANGE;
   mask |= PRE_FORCE;
@@ -90,27 +102,30 @@ int FixImageCharge::setmask() {
 
 /* ---------------------------------------------------------------------- */
 
-void FixImageCharge::init() {
+void FixImageQ2D::init() {
   build_img_parents();
-  // assume fixed charge model. Only assign image charges once
+  // assume fixed charge model for electrolytes. Only assign image charges once
   assign_img_charges();
   update_img_positions();
+  update_electrode_charges();
 }
 
 /* ---------------------------------------------------------------------- */
 
-void FixImageCharge::pre_exchange(int /*vflag*/) {
+void FixImageQ2D::pre_exchange(int /*vflag*/) {
   update_img_positions();
+  update_electrode_charges();
 }
 
 /* ---------------------------------------------------------------------- */
 
-void FixImageCharge::pre_force(int /*vflag*/) {
+void FixImageQ2D::pre_force(int /*vflag*/) {
   update_img_positions();
+  update_electrode_charges();
 }
 
 /* ---------------------------------------------------------------------- */
-void FixImageCharge::build_img_parents() {
+void FixImageQ2D::build_img_parents() {
   int *flag_parents = new int[atom->natoms + 1];
   int *flag_imgs = new int[atom->natoms + 1];
   int *flag_parents_local = new int[atom->natoms + 1];
@@ -134,7 +149,7 @@ void FixImageCharge::build_img_parents() {
   MPI_Allreduce(flag_imgs_local, flag_imgs, atom->natoms + 1, MPI_INT, MPI_SUM, world);
 
   for (int i = 1; i < atom->natoms + 1; i++) {
-    img_parent[i] = -1;
+    img_parent[i] = 0; // set the default of image_parent to 0. since the tag of atoms starts from 1. negative value is used for anode iamges
     if (flag_parents[i] == 1) tag_parents.push_back(i);
     if (flag_imgs[i] == 1) tag_imgs.push_back(i);
   }
@@ -148,13 +163,14 @@ void FixImageCharge::build_img_parents() {
 //  }
 
   for (int i = 0; i < tag_parents.size(); i++) {
-    img_parent[tag_imgs[i]] = tag_parents[i];
+    img_parent[tag_imgs[i]] = tag_parents[i]; // images on cathode
+    img_parent[tag_imgs[i] + tag_parents.size()] = -tag_parents[i]; // images on anode. set the tag of parent to negative so can be distinguished
   }
 }
 
 /* ---------------------------------------------------------------------- */
 
-void FixImageCharge::assign_img_charges() {
+void FixImageQ2D::assign_img_charges() {
   int tag, tag_parent;
   int *mask = atom->mask;
   double *q = atom->q;
@@ -177,8 +193,8 @@ void FixImageCharge::assign_img_charges() {
   for (int ii = 0; ii < atom->nlocal; ii++) {
     tag = atom->tag[ii];
     tag_parent = img_parent[tag];
-    if (tag_parent != -1) {
-      q[ii] = -charge[tag_parent];
+    if (tag_parent != 0) {
+      q[ii] = -charge[abs(tag_parent)]; // use abs because tag_parent can be negative for anodes
     }
   }
 
@@ -188,7 +204,7 @@ void FixImageCharge::assign_img_charges() {
 
 /* ---------------------------------------------------------------------- */
 
-void FixImageCharge::update_img_positions() {
+void FixImageQ2D::update_img_positions() {
   int tag, tag_parent;
   int *mask = atom->mask;
   double **x = atom->x;
@@ -221,20 +237,60 @@ void FixImageCharge::update_img_positions() {
 
   // update the xyz of image particles
   for (int ii = 0; ii < atom->nlocal; ii++) {
-    tag = atom->tag[ii];
-    tag_parent = img_parent[tag];
-    if (tag_parent != -1) {
-      xyz_img_tmp[0] = x[ii][0];
-      xyz_img_tmp[1] = x[ii][1];
-      xyz_img_tmp[2] = x[ii][2];
-      x[ii][0] = xyz[tag_parent][0];
-      x[ii][1] = xyz[tag_parent][1];
-      x[ii][2] = 2 * z0 - xyz[tag_parent][2];
-      // make sure the new coordinate of images is in the correct periodic box
-       domain->remap_near(x[ii], xyz_img_tmp);
+    if (mask[ii] & group2bit) {
+      tag = atom->tag[ii];
+      tag_parent = img_parent[tag];
+        xyz_img_tmp[0] = x[ii][0];
+        xyz_img_tmp[1] = x[ii][1];
+        xyz_img_tmp[2] = x[ii][2];
+        if (tag_parent > 0){
+          x[ii][0] = xyz[tag_parent][0];
+          x[ii][1] = xyz[tag_parent][1];
+          x[ii][2] = 2 * z_cathode - xyz[tag_parent][2];
+        }
+        else if (tag_parent < 0){
+          x[ii][0] = xyz[-tag_parent][0];
+          x[ii][1] = xyz[-tag_parent][1];
+          x[ii][2] = 2 * z_anode - xyz[-tag_parent][2];
+        }
+        else
+          error->all(FLERR, "parent tag missing for image charge");
+
+        // make sure the new coordinate of images is in the correct periodic box
+        domain->remap_near(x[ii], xyz_img_tmp);
 //      if (abs(x[ii][0] - xyz_tmp[0]) > 0.01 or abs(x[ii][1] - xyz_tmp[1]) > 0.01 or abs(x[ii][2] - xyz_tmp[2]) > 0.01)
 //        printf("old and new xyz: %d %d, %f %f %f, %f %f %f\n", tag, tag_parent,
 //               xyz_tmp[0], xyz_tmp[1], xyz_tmp[2], x[ii][0], x[ii][1], x[ii][2]);
     }
   }
+}
+
+void FixImageQ2D::update_electrode_charges() {
+  int *mask = atom->mask;
+  double **x = atom->x;
+  double *q = atom->q;
+  double z;
+  double q_electrodes[2];
+  double q_electrodes_local[2] = {0, 0};
+
+  for (int ii = 0; ii < atom->nlocal; ii++) {
+    if (mask[ii] & groupbit) {
+      z = x[ii][2];
+      q_electrodes_local[0] += q[ii] * (z - z_cathode) / z_span;
+      q_electrodes_local[1] += q[ii] * (z_anode - z) / z_span;
+    }
+  }
+  MPI_Allreduce(q_electrodes_local, q_electrodes, 2, MPI_DOUBLE, MPI_SUM, world);
+
+  double q_voltage = V * domain->xprd * domain->yprd / z_span * 8.85419 / 1.60218 * 0.001;
+
+  for (int ii = 0; ii < atom->nlocal + atom->nghost; ii++) {
+    if (mask[ii] & groupbit_cathode)
+      q[ii] = (q_electrodes[0] + q_voltage) / n_cathode;
+    else if (mask[ii] & groupbit_anode)
+      q[ii] = (q_electrodes[1] - q_voltage) / n_anode;
+  }
+
+  // reset KSpace charges
+  if (force->kspace) force->kspace->qsum_qsq();
 }
---
+++ src/USER-GONG/fix_imageq2d.h
@@ -0,0 +1,81 @@
+/* -*- c++ -*- ----------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifdef FIX_CLASS
+
+FixStyle(imageq2d,FixImageQ2D)
+
+#else
+
+#ifndef LMP_FIX_IMAGE_Q2D_H
+#define LMP_FIX_IMAGE_Q2D_H
+
+#include "fix.h"
+
+namespace LAMMPS_NS {
+
+class FixImageQ2D : public Fix {
+ public:
+  FixImageQ2D(class LAMMPS *, int, char **);
+  ~FixImageQ2D();
+  int setmask();
+  void init();
+  void pre_exchange(int);
+  void pre_force(int);
+
+ private:
+  int igroup2,group2bit;
+  int igroup_cathode, groupbit_cathode;
+  int igroup_anode, groupbit_anode;
+
+
+  double V, z_cathode, z_anode, z_span; // voltage drop, position of cathode and anode
+  int n_cathode, n_anode; // number of atoms in cathode and anode
+  tagint * img_parent;
+  double ** xyz;
+  double ** xyz_local;
+  double * xyz_img_tmp;
+  double * charge;
+  double * charge_local;
+  void build_img_parents();
+  void assign_img_charges();
+  void update_img_positions();
+  void update_electrode_charges();
+};
+
+}
+
+#endif
+#endif
+
+/* ERROR/WARNING messages:
+
+E: Illegal ... command
+
+Self-explanatory.  Check the input script syntax and compare to the
+documentation for the command.  You can use -echo screen as a
+command-line option when running LAMMPS to see the offending line.
+
+E: R0 < 0 for fix spring command
+
+Equilibrium spring length is invalid.
+
+E: Fix spring couple group ID does not exist
+
+Self-explanatory.
+
+E: Two groups cannot be the same in fix spring couple
+
+Self-explanatory.
+
+*/
